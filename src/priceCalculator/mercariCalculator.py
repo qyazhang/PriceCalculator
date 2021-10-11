dae676715b66d9dcecbce8aa592c621cc323a44d
Qiyang Zhang <qyazhang1995@gmail.com>
2021-10-11 16:21:45 +0900

    change driver config

--- src/priceCalculator/mercariCalculator.py
+++ src/priceCalculator/mercariCalculator.py
@@ -17,7 +17,12 @@ def calculateMercariPrice(request, url):
         print("input is not valid url: " + url)
         return model
 
-    driver = webdriver.Remote("http://selenium:4444/wd/hub", DesiredCapabilities.CHROME)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--window-size=1420,1080')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Remote("http://selenium:4444/wd/hub", options=chrome_options)
     driver.get(url)
     time.sleep(1)
 
